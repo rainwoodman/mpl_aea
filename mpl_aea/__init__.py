0dd5e613a29ca2ae45d74398bbe78ebf62ec797a
Yu Feng <rainwoodman@gmail.com>
2016-08-31 15:13:22 -0700

    Steal matplotlib's geo.
    We'll hack in a histmap function with @paulinevielzeuf

--- mpl_aea/__init__.py
+++ mpl_aea/__init__.py
@@ -1,6 +1,8 @@
 from matplotlib.projections import register_projection
 
 from .aea import AlbersEqualAreaAxes
+from .geo import MollweideAxes
 # Now register the projection with matplotlib so the user can select
 # it.
 register_projection(AlbersEqualAreaAxes)
+register_projection(MollweideAxes)
